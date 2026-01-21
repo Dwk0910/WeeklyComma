846c2b0257a62aa7725e50d68a195b6156484dae
Dongwan Kim <dongwan204@naver.com>
2026-01-21 20:21:31 +0900

    Feature: Initial Configuration

--- frontend/eslint.config.js
+++ frontend/eslint.config.js
@@ -3,10 +3,13 @@ import globals from 'globals'
 import reactHooks from 'eslint-plugin-react-hooks'
 import reactRefresh from 'eslint-plugin-react-refresh'
 import tseslint from 'typescript-eslint'
+import prettier from 'eslint-plugin-prettier'
 import { defineConfig, globalIgnores } from 'eslint/config'
+import eslintConfigPrettier from 'eslint-config-prettier'
 
 export default defineConfig([
   globalIgnores(['dist']),
+
   {
     files: ['**/*.{ts,tsx}'],
     extends: [
@@ -14,7 +17,20 @@ export default defineConfig([
       tseslint.configs.recommended,
       reactHooks.configs.flat.recommended,
       reactRefresh.configs.vite,
+
+      // ⭐ Prettier랑 충돌 나는 ESLint 규칙 OFF
+      eslintConfigPrettier,
     ],
+    plugins: {
+      prettier,
+    },
+    rules: {
+      // ⭐ Prettier 결과를 ESLint 에러로
+      'prettier/prettier': 'error',
+
+      // ⭐ 세미콜론 안 쓰면 에러
+      semi: ['error', 'always'],
+    },
     languageOptions: {
       ecmaVersion: 2020,
       globals: globals.browser,
