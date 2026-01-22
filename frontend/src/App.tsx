25b2c72936b62e6997d2dbfc346deecde2ceca03
Dongwan Kim <dongwan204@naver.com>
2026-01-23 02:33:53 +0900

    Feature: Added react-router-dom

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,14 +1,28 @@
+import { Routes, Route } from "react-router-dom";
+
 import PreTopBar from "./component/PreTopBar";
 import TopBar from "./component/TopBar";
+import Footer from "./component/Footer.tsx";
+
+import Main from "./pages/Main.tsx";
 
 export default function App() {
     return (
-        <div className={"w-full flex justify-center"}>
-            <div className={"w-full absolute bg-gray-200 h-10 z-0"} />
-            <div className={"w-300 z-10"}>
-                <PreTopBar />
-                <TopBar />
+        <>
+            <div className={"w-full flex relative flex-col items-center"}>
+                <div className={"w-full absolute bg-gray-200 h-10 z-0"} />
+                <div className={"w-300 z-10"}>
+                    <PreTopBar />
+                    <TopBar />
+                    <Routes>
+                        <Route index element={<Main />} />
+                    </Routes>
+                </div>
+                <div className={"w-full border-t border-gray-400 mt-15"} />
+                <div className={"w-300 z-10"}>
+                    <Footer />
+                </div>
             </div>
-        </div>
+        </>
     );
 }
