7a99e2ca301a68ec30454fdc5e4963770950155c
Dongwan Kim <dongwan204@naver.com>
2026-01-23 21:15:17 +0900

    Feature: Backend: Initial Commit

---
+++ backend/src/main/java/org/neatore/weeklycomma/WeeklyComma.java
@@ -0,0 +1,11 @@
+package org.neatore.weeklycomma;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WeeklyComma {
+    public static void main(String[] args) {
+        SpringApplication.run(WeeklyComma.class, args);
+    }
+}
