58f7c239607c03a031be08acbbf75698f3fdda74
Dongwan Kim <dongwan204@naver.com>
2026-02-28 20:26:36 +0900

    Feature: Framing backend connection

--- backend/src/main/java/org/neatore/weeklycomma/WeeklyComma.java
+++ backend/src/main/java/org/neatore/weeklycomma/WeeklyComma.java
@@ -2,10 +2,23 @@ package org.neatore.weeklycomma;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.ResponseEntity;
 
 @SpringBootApplication
 public class WeeklyComma {
     public static void main(String[] args) {
         SpringApplication.run(WeeklyComma.class, args);
+    }
+}
+
+@CrossOrigin(origins = { "http://localhost:5173" })
+@RestController
+class SysController {
+    @GetMapping("/health")
+    public ResponseEntity<String> health() {
+        return ResponseEntity.ok("OK");
     }
 }
--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,4 +1,6 @@
+import axios from "axios";
 import { Routes, Route } from "react-router-dom";
+import { useState } from "react";
 
 import PreTopBar from "./component/PreTopBar";
 import TopBar from "./component/TopBar";
@@ -6,10 +8,27 @@ import Footer from "./component/Footer.tsx";
 
 import Main from "./pages/Main.tsx";
 
+export const BACKEND_ADDRESS = import.meta.env.VITE_API_BACKEND_ADDRESS;
+
 export default function App() {
-    return (
+    const [backendErr, setBackendErr] = useState<{
+        error: boolean;
+        info: string | null;
+    }>({
+        error: false,
+        info: null
+    });
+
+    // Backend server check
+    (async () => {
+        await axios(BACKEND_ADDRESS + "/health")
+            .then((res) => setBackendErr((prev) => ({ ...prev, error: res.data != "OK" })))
+            .catch((err) => setBackendErr({ error: true, info: err.toString() }));
+    })();
+
+    return !backendErr ? (
         <>
-            <div className={"w-full relative flex flex-col mx-auto"}>
+            <div className={"w-full relative flex flex-col"}>
                 <div className={"w-full absolute bg-gray-200 h-10 z-0"} />
                 <div className={"w-300 mx-auto z-10"}>
                     <div className={"w-300"}>
@@ -22,6 +41,32 @@ export default function App() {
                     <div className={"w-full border-t border-gray-400 mt-15"} />
                     <div className={"w-300 z-10"}>
                         <Footer />
+                    </div>
+                </div>
+            </div>
+        </>
+    ) : (
+        <>
+            <div className={"w-full h-screen flex justify-center items-center"}>
+                <div
+                    className={
+                        "w-150 h-100 border border-gray-400 rounded-xs flex flex-col font-suite"
+                    }
+                >
+                    <div className={"text-red-500 text-[2rem] mt-7 ml-7 -mb-2"}>ERROR!</div>
+                    <span className={"ml-7"}>SERVER ERROR OCCURED</span>
+                    <div className={"mt-3 ml-7"}>
+                        서버와의 통신 오류가 발생했습니다.
+                        <br />
+                        서비스 점검 중이거나, 일시적인 현상일 수 있습니다.
+                        <br />
+                        <div className={"mt-2"}>오류가 지속되면 관리자에게 문의 바랍니다.</div>
+                    </div>
+                    <div className={"mx-7 mt-5 mb-1 border-b border-b-gray-400"} />
+                    <div className={"font-mono ml-7"}>
+                        ERROR INFO
+                        <br />
+                        {backendErr.info}
                     </div>
                 </div>
             </div>
