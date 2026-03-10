f1a480d5d8de0fa9eee8b075944fe551dd9af2a1
Dongwan Kim <dongwan204@naver.com>
2026-03-10 22:26:47 +0900

    Code style

--- backend/src/main/java/org/neatore/weeklycomma/interceptors/AuthenticationInterceptor.java
+++ backend/src/main/java/org/neatore/weeklycomma/interceptors/AuthenticationInterceptor.java
@@ -1,6 +1,5 @@
 package org.neatore.weeklycomma.interceptors;
 
-import lombok.RequiredArgsConstructor;
 import org.jetbrains.annotations.NotNull;
 
 import org.neatore.weeklycomma.annotations.RequiresAuthorization;
@@ -12,6 +11,8 @@ import org.springframework.stereotype.Component;
 import org.springframework.web.cors.CorsUtils;
 import org.springframework.web.method.HandlerMethod;
 import org.springframework.web.servlet.HandlerInterceptor;
+
+import lombok.RequiredArgsConstructor;
 
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
