de4e76316e8e2790dea1818738b11bf664eb7dbd
Dongwan Kim <dongwan204@naver.com>
2026-03-13 00:20:23 +0900

    DEL(cause prev-commit): No longer required

--- frontend/src/pages/admin/ManageBoards.tsx
+++ frontend/src/pages/admin/ManageBoards.tsx
@@ -1,11 +1,6 @@
-import { useState } from "react";
 import Component, { Title, SubTitle } from "./lib_component/Component";
 
 export default function ManageBoards() {
-    const [selectedBoard, setSelectedBoard] = useState<
-        "Notifications" | "Events" | "GeneralRecommands"
-    >("Notifications");
-
     return (
         <Component>
             <Title>게시판 글 관리</Title>
