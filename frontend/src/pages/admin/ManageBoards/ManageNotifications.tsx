db8ebbbbeda9e3a5615f85a3a86084fe31fc24af
Dongwan Kim <dongwan204@naver.com>
2026-04-05 03:21:50 +0900

    Feature: post list viewer

--- frontend/src/pages/admin/ManageBoards/ManageNotifications.tsx
+++ frontend/src/pages/admin/ManageBoards/ManageNotifications.tsx
@@ -1,5 +1,5 @@
 import Component, { Title } from "../lib_component/Component";
-import BACKEND_ADDRESS from "../../../App";
+import { BACKEND_ADDRESS } from "../../../App";
 
 import axios from "axios";
 import { clsx } from "clsx";
@@ -7,44 +7,79 @@ import { useState, useEffect } from "react";
 
 import loading_gif from "../../../assets/loading.gif";
 
+type Post = {
+    id: number;
+    title: string;
+    author: string;
+    createdAt: number;
+    updatedAt: number;
+    isPinned: boolean;
+    type: "NOTICE" | "EVENT";
+    content: string;
+};
+
 export default function ManageNotifications() {
     const [isLoading, setIsLoading] = useState<boolean>(true);
+    const [posts, setPosts] = useState<Array<Post>>([]);
     const [selectedPosts, setSelectedPosts] = useState<Array<string>>([]);
 
     useEffect(() => {
-        axios.get(BACKEND_ADDRESS + "post/getAllPost").then((res) => {
-            console.log(res.data);
+        axios.get(BACKEND_ADDRESS + "post/getAllPosts/NOTICE").then((res) => {
+            const postList: Post[] = res.data;
+            setPosts(
+                postList.sort((a, b) => {
+                    if (a.isPinned !== b.isPinned) return a.isPinned ? -1 : 1;
+                    return b.updatedAt - a.updatedAt;
+                })
+            );
             setIsLoading(false);
         });
     }, []);
+
+    const menuBtnStyle: (isVariable: boolean, ...color: string[]) => string = (
+        isVariable,
+        ...color
+    ) => {
+        return isVariable
+            ? clsx(
+                  Array.of(
+                      "p-2 ml-2 mb-2 text-white font-suite w-25 rounded-xs",
+                      "transition-colors duration-200 ease-in-out",
+                      selectedPosts.length != 0
+                          ? `${color[0]} cursor-pointer`
+                          : `${color[1]} cursor-not-allowed`
+                  )
+              )
+            : clsx(
+                  Array.of(
+                      `${color[0]} p-2 ml-2 mb-2 text-white font-suite w-25 rounded-xs cursor-pointer`
+                  )
+              );
+    };
 
     return (
         <Component>
             <Title>공지 관리</Title>
             <div className={"flex"}>
-                <button
-                    className={
-                        "p-2 mb-2 bg-blue-500 text-white font-suite w-25 rounded-xs cursor-pointer"
-                    }
-                >
+                <button className={menuBtnStyle(false, "bg-green-600")} style={{ marginLeft: 0 }}>
                     새 글 작성
                 </button>
                 <button
-                    className={clsx(
-                        "p-2 ml-2 mb-2 text-white font-suite w-25 rounded-xs cursor-pointer",
-                        "transition-colors duration-200 ease-in-out",
-                        selectedPosts.length != 0 ? "bg-blue-500" : "bg-neutral-400"
-                    )}
+                    className={menuBtnStyle(false, "bg-blue-500")}
+                    onClick={() => setSelectedPosts(posts.map((i) => i.id.toString()))}
                 >
-                    선택 고정
+                    전체 선택
                 </button>
                 <button
-                    className={clsx(
-                        "p-2 ml-2 mb-2 text-white font-suite w-25 rounded-xs cursor-pointer",
-                        "transition-colors duration-200 ease-in-out",
-                        selectedPosts.length != 0 ? "bg-red-400" : "bg-neutral-400"
-                    )}
+                    className={menuBtnStyle(true, "bg-blue-500", "bg-neutral-400")}
+                    onClick={() => setSelectedPosts([])}
                 >
+                    선택 취소
+                </button>
+                <button className={menuBtnStyle(true, "bg-blue-500", "bg-neutral-400")}>
+                    선택 고정
+                </button>
+                <button className={menuBtnStyle(true, "bg-red-400", "bg-neutral-400")}>
                     선택 삭제
                 </button>
                 <span
@@ -56,20 +91,22 @@ export default function ManageNotifications() {
                     선택한 글 수 : {selectedPosts.length}
                 </span>
             </div>
-            <div className={"flex border-b pb-8 px-3 border-b-gray-500 w-full h-5 mt-2 font-suite"}>
-                <style>
-                    {`
-                        .notice-table-header {
-                            text-align: center;
-                        }
-                    `}
-                </style>
-                <div className={"notice-table-header w-8"}>선택</div>
-                <div className={"notice-table-header w-40"}>작성자</div>
-                <div className={"notice-table-header w-90"}>이름</div>
-                <div className={"notice-table-header w-20"}>작성번호</div>
-                <div className={"notice-table-header w-35"}>작성일</div>
-                <div className={"notice-table-header w-35"}>수정일</div>
+            <style>{`
+            .notice-table > div {
+                text-align: center;
+            }
+            `}</style>
+            <div
+                className={
+                    "flex border-b pb-8 border-b-gray-500 w-full h-5 mt-2 font-suite notice-table"
+                }
+            >
+                <div className={"w-15"}>선택</div>
+                <div className={"w-15"}>작성번호</div>
+                <div className={"w-35"}>작성자</div>
+                <div className={"w-110"}>이름</div>
+                <div className={"w-30"}>작성일</div>
+                <div className={"w-30"}>수정일</div>
             </div>
             {isLoading ? (
                 <div className={"w-full h-100 flex flex-col justify-center items-center"}>
@@ -77,7 +114,56 @@ export default function ManageNotifications() {
                     <img alt={"loading"} src={loading_gif} className={"w-10 mt-5"} />
                 </div>
             ) : (
-                <div className={"w-full flex flex-col"}></div>
+                <div className={"w-full flex flex-col"}>
+                    {posts.map((item) => (
+                        <div
+                            className={clsx(
+                                "w-full flex border-b py-1",
+                                item.isPinned && "bg-red-500/8"
+                            )}
+                            key={`post-list-button-${item.id}`}
+                        >
+                            <div className={"w-15 flex justify-center"}>
+                                <input
+                                    type={"checkbox"}
+                                    checked={selectedPosts.includes(item.id.toString())}
+                                    onChange={(e) => {
+                                        if (e.target.checked)
+                                            setSelectedPosts((prev) => [
+                                                ...prev,
+                                                item.id.toString()
+                                            ]);
+                                        else
+                                            setSelectedPosts((prev) =>
+                                                prev.filter((i) => i != item.id.toString())
+                                            );
+                                    }}
+                                />
+                            </div>
+                            <div className={"w-15 font-suite text-center"}>{item.id}</div>
+                            <div className={"w-35 text-center"}>{item.author}</div>
+                            <div className={"w-110 cursor-pointer hover:underline"}>
+                                {item.title}
+                            </div>
+                            {(() => {
+                                // JS Date 클래스는 밀리초 단위이므로 초 단위인 createdAt에 1000을 곱해줍니다.
+                                const createdAtDate: Date = new Date(item.createdAt * 1000);
+                                const updatedAtDate: Date = new Date(item.updatedAt * 1000);
+
+                                return (
+                                    <>
+                                        <div className={"w-30 font-suite text-center"}>
+                                            {`${createdAtDate.getFullYear()}.${createdAtDate.getMonth() + 1}.${createdAtDate.getDate()}.`}
+                                        </div>
+                                        <div className={"w-30 font-suite text-center"}>
+                                            {`${updatedAtDate.getFullYear()}.${updatedAtDate.getMonth() + 1}.${updatedAtDate.getDate()}.`}
+                                        </div>
+                                    </>
+                                );
+                            })()}
+                        </div>
+                    ))}
+                </div>
             )}
         </Component>
     );
