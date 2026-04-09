7d8df8f01fb180c8e6579fbbd30d010474da9ef8
Dongwan Kim <dongwan204@naver.com>
2026-04-10 07:02:52 +0900

    Featrue: Apply Tiptap WYSIWYG text editor library

--- frontend/src/pages/admin/ManageBoards/Editor.tsx
+++ frontend/src/pages/admin/ManageBoards/Editor.tsx
@@ -1,5 +1,17 @@
+import { useState } from "react";
+
 import { type PostType } from "./ManageNotifications.tsx";
+
 import { AnimatePresence, motion } from "framer-motion";
+import { clsx } from "clsx";
+
+// Modules for WYSIWYG editor
+import { useEditor, EditorContent } from "@tiptap/react";
+import { Table, TableCell, TableHeader, TableRow } from "@tiptap/extension-table";
+import StarterKit from "@tiptap/starter-kit";
+
+// Editor menu icon
+import { FaBold, FaUnderline, FaItalic } from "react-icons/fa6";
 
 export default function Editor({
     articleType,
@@ -8,6 +20,28 @@ export default function Editor({
     articleType: PostType;
     visible: boolean;
 }) {
+    const [title, setTitle] = useState<string>("");
+    const [content, setContent] = useState<string>("");
+
+    const editor = useEditor({
+        extensions: [
+            StarterKit,
+            Table.configure({ resizable: true }),
+            TableRow,
+            TableCell,
+            TableHeader
+        ],
+
+        content: content,
+        onUpdate: ({ editor }) => setContent(editor.getHTML()),
+
+        editorProps: {
+            attributes: {
+                class: "w-full h-95 p-3 pt-0 outline-none"
+            }
+        }
+    });
+
     return (
         <AnimatePresence>
             {visible && (
@@ -16,7 +50,7 @@ export default function Editor({
                         height: 0
                     }}
                     animate={{
-                        height: "300px"
+                        height: "500px"
                     }}
                     exit={{
                         height: 0
@@ -27,7 +61,49 @@ export default function Editor({
                     }}
                     style={{ overflow: "hidden" }}
                 >
-                    <span className={"ml-2 font-seoulnamsan"}>글 작성기</span>
+                    <input
+                        type={"text"}
+                        className={
+                            "ml-2 w-[95%] px-2 h-10 border-[1.5px] border-b-0 border-gray-300 outline-none"
+                        }
+                        value={title}
+                        onChange={(e) => setTitle(e.target.value)}
+                        placeholder={"제목"}
+                    />
+                    <div className={"w-[95%] h-110 border-[1.5px] border-gray-300 mx-2"}>
+                        <div className={"flex w-full h-9 p-2 ml-1 mb-3"}>
+                            <div
+                                className={clsx(
+                                    "w-8.5 h-8.5 bg-gray-500 flex justify-center items-center text-white rounded-xs",
+                                    "cursor-pointer transition-[scale] hover:scale-110"
+                                )}
+                                onClick={() => editor.chain().focus().toggleBold().run()}
+                            >
+                                <FaBold />
+                            </div>
+                            <div
+                                className={clsx(
+                                    "w-8.5 h-8.5 ml-2 bg-gray-500 flex justify-center items-center text-white rounded-xs",
+                                    "cursor-pointer transition-[scale] hover:scale-110"
+                                )}
+                                onClick={() => editor.chain().focus().toggleUnderline().run()}
+                            >
+                                <FaUnderline />
+                            </div>
+                            <div
+                                className={clsx(
+                                    "w-8.5 h-8.5 ml-2 bg-gray-500 flex justify-center items-center text-white rounded-xs",
+                                    "cursor-pointer transition-[scale] hover:scale-110"
+                                )}
+                                onClick={() => editor.chain().focus().toggleItalic().run()}
+                            >
+                                <FaItalic />
+                            </div>
+                        </div>
+                        <div className={"overflow-y-auto"}>
+                            <EditorContent editor={editor} />
+                        </div>
+                    </div>
                 </motion.div>
             )}
         </AnimatePresence>
