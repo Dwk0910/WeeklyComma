868339976e14306b645d38b49e7398e7a13ceb32
Dongwan Kim <dongwan204@naver.com>
2026-04-29 02:17:43 +0900

    Style: Removed memo

--- frontend/src/index.tsx
+++ frontend/src/index.tsx
@@ -6,7 +6,6 @@ import { BrowserRouter } from "react-router-dom";
 import App from "./App.tsx";
 import "./index.css";
 
-// 백엔드로 날리는 모든 요청에 쿠키를 포함하도록 설정
 axios.defaults.withCredentials = true;
 
 createRoot(document.getElementById("root")!).render(
