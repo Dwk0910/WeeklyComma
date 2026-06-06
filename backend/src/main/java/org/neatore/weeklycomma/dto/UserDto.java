a403adc179974f77b4bfbe95903119479b7de42d
Dongwan Kim <dongwan204@naver.com>
2026-06-07 00:24:24 +0900

    Feature: UserDto fields used when the authentication request comes has been separated into AuthDto

--- backend/src/main/java/org/neatore/weeklycomma/dto/UserDto.java
+++
@@ -1,8 +0,0 @@
-package org.neatore.weeklycomma.dto;
-
-import jakarta.validation.constraints.NotBlank;
-
-public class UserDto {
-    public record LoginRequest(@NotBlank String email, @NotBlank String password) {}
-    public record NaverOAuthRequest(@NotBlank String auth_code, @NotBlank String redirect_uri, @NotBlank String state) {}
-}
