110f646946a83bb74723e01b99450012f344458d
Dongwan Kim <dongwan204@naver.com>
2026-06-08 00:44:06 +0900

    Fix: Allowed the 'PATCH' method.

--- backend/src/main/java/org/neatore/weeklycomma/WebSecurityConfig.java
+++ backend/src/main/java/org/neatore/weeklycomma/WebSecurityConfig.java
@@ -27,7 +27,7 @@ public class WebSecurityConfig {
     public CorsConfigurationSource corsConfigurationSource() {
         CorsConfiguration config = new CorsConfiguration();
         config.setAllowedOrigins(List.of("http://localhost:5173", "https://www.weeklycomma.kro.kr"));
-        config.setAllowedMethods(List.of("GET", "POST", "PUT", "DELETE"));
+        config.setAllowedMethods(List.of("GET", "POST", "PUT", "PATCH", "DELETE"));
         config.setAllowedHeaders(List.of("*"));
         config.setAllowCredentials(true);
 
