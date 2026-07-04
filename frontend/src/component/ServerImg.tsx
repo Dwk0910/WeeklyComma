c97beb790c3aaccdf9d4aa4e4bf4b197b874b847
Dongwan Kim <dongwan204@naver.com>
2026-07-05 05:06:45 +0900

    Feature: Improved performance (Using 'blob')

--- frontend/src/component/ServerImg.tsx
+++ frontend/src/component/ServerImg.tsx
@@ -11,11 +11,22 @@ export default function ServerImg({ fileId, alt, ...props }: ServerImgProps) {
     const [src, setSrc] = useState<string>();
 
     useEffect(() => {
+        let urlLocal = "";
+
         const fetchImage = async () => {
-            const response = await axios.get(BACKEND_ADDRESS + `files/base64/${fileId}`);
-            setSrc(response.data);
+            const response = await axios.get(BACKEND_ADDRESS + `files/${fileId}`, {
+                responseType: "blob"
+            });
+
+            const url = URL.createObjectURL(response.data);
+            urlLocal = url;
+            setSrc(url);
         };
         void fetchImage();
+
+        return () => {
+            if (urlLocal) URL.revokeObjectURL(urlLocal);
+        };
     }, [fileId]);
 
     return <img src={src} alt={alt!} {...props} />;
